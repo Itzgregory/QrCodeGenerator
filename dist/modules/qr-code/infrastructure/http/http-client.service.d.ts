3070ad997472223ccaa31085a73bb29057566d02
Itzgregory <gregorychuks0@gmail.com>
2025-06-13 09:30:00 +0100

    prod

--- dist/modules/qr-code/infrastructure/http/http-client.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { HttpService } from '@nestjs/axios';
-export declare class HttpClientService {
-    private readonly httpService;
-    constructor(httpService: HttpService);
-    get(url: string): Promise<any>;
-}
--- dist/modules/qr-code/infrastructure/view/pages/camera-page.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function cameraPage(): string;
